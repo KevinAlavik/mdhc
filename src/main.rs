0f9dacb9e0972b5706b18b099e95a0bc4c0ad7ba
Kevin Alavik <kevin@alavik.se>
2023-11-23 08:28:53 +0000

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::fs;
 
+enum Tokens {
+   NULL,
+   EOF
+}
+
 fn main() {
     let file_path = "test.md";
 
     let content = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
+
+    let nullToken = Tokens::NULL;
 
     let c: Vec<Vec<char>> = content.lines().map(|line| line.chars().collect()).collect();
 
